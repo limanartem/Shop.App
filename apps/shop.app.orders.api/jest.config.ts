131428dcefdc5c7149b92971cffbcabbbe36cce9
Artem Liman <artem.liman@gendigital.com>
2023-11-27 18:27:07 +0100

    fixed jest test

--- apps/shop.app.orders.api/jest.config.ts
+++ apps/shop.app.orders.api/jest.config.ts
@@ -1,14 +1,22 @@
-export const preset = 'ts-jest';
-export const coverageThreshold = {
-  global: {
-    branches: 85,
-    functions: 90,
-    lines: 90,
-    statements: 90,
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  // collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
   },
+  detectOpenHandles: true,
+  verbose: true,
+  passWithNoTests: true,
+  roots: ['src/'],
+  setupFiles: ['<rootDir>/src/.jest/globalSetup.ts'],
+  // runner: 'groups',
 };
-export const detectOpenHandles = true;
-export const verbose = true;
-export const passWithNoTests = true;
-export const roots = ['src/'];
-export const setupFiles = ['<rootDir>/src/.jest/globalSetup.ts'];
+
+export default config;
