53440fb9b4a1cdb232d893e7421a83e51ac64aba
Artem Liman <artem.liman@gendigital.com>
2023-12-04 14:39:05 +0100

    added dotenv for dev setup

--- apps/shop.app.orders.api/src/index.ts
+++ apps/shop.app.orders.api/src/index.ts
@@ -1,6 +1,6 @@
 import { start } from './express/server';
 
-const { WEB_SERVER_PORT = 3001 } = process.env;
+const { WEB_SERVER_PORT } = process.env;
 
 start().listen(WEB_SERVER_PORT, () => {
   console.log(`Server is running on port ${WEB_SERVER_PORT}`);
