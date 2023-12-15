635856d4ff59020bbf7e01924d331ca10d79d74d
Artem Liman <artem.liman@gendigital.com>
2023-12-15 15:11:59 +0100

    Add mobile app files and update dependencies for ts

--- apps/shop.app.mobile/App.tsx
+++ apps/shop.app.mobile/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
