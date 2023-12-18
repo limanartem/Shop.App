668c657fd5bbc4498d93045c8f4ea30898eef2de
Artem Liman <artem.liman@gendigital.com>
2023-12-18 18:59:59 +0100

    Update asset URL and supertokens-auth-react version

--- apps/shop.app.web/src/utils/product-utils.ts
+++ apps/shop.app.web/src/utils/product-utils.ts
@@ -3,8 +3,9 @@ import { env } from '../config/environment';
 
 const { REACT_APP_CDN_URL } = env;
 
+export const assetUrl = (path: string): string => `${REACT_APP_CDN_URL}/${path}`;
+
 export const getProductImage = (product: ProductItem, index: number = 1): string =>
   `${REACT_APP_CDN_URL}/shop-assets/products/thumbnails/${product.id}-${index}.png`;
 
 export const ProductFallbackImage = `${REACT_APP_CDN_URL}/shop-assets/products/no-product-image.png`;
-
