87e860199e4ed81a1cc1f95e95ab2835c97eb480
Artem Liman <artem.liman@gendigital.com>
2023-12-22 21:25:05 +0100

    Update tsconfig.json and package.json, add codegen.ts and modify index.ts

--- apps/shop.app.shopping-cart.api/src/index.ts
+++ apps/shop.app.shopping-cart.api/src/index.ts
@@ -3,13 +3,14 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { readFileSync } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { Resolvers } from './generated/graphql';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const typeDefs = readFileSync(path.join(__dirname, 'schema.graphql'), { encoding: 'utf-8' });
 
-const resolvers = {
+const resolvers: Resolvers = {
   Query: {
     shoppingCart: () => {
       return {
